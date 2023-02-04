bef7fd543ff0d206a5e0ed45ba228e5b53078254
Makhkambek <okeydokeykokki@gmail.com>
2023-02-04 18:56:23 +0500

    Initial Commit

---
+++ About me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About me
+//
+//  Created by Махкамбек Тешабаев on 04.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
