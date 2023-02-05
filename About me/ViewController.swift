507d8c8bd27680bc50f3e9b4d2ce099072442a3d
Makhkambek <okeydokeykokki@gmail.com>
2023-02-05 12:58:39 +0500

    first commit

--- About me/ViewController.swift
+++ About me/ViewController.swift
@@ -10,10 +10,37 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func pressToButton(_ sender: Any) {
+        openUrl(urlStr: "https://www.instagram.com/teshabayevv/")
+    }
+     
+    func openUrl(urlStr: String!) {
+        if let url = URL(string: urlStr), !url.absoluteString.isEmpty {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+        
+    }
+    
+    
+    
+    
+    
+    @IBAction func buttonPress(_ sender: Any) {
+        openUrl(urlStr: "https://github.com/Makhkambek")
+    }
+    
+    
+    
+    
+    
+    
 }
 
